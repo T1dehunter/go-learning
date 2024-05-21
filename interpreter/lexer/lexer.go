682d075515f13c87f8fa50079a6b4a20cce5043f
aleksey.shumlyaev <aleksey.shumlyaev@rhinocodes.com>
2024-05-22 01:40:38 +0300

    refactor(interpreter): rework lexer

--- interpreter/lexer/lexer.go
+++ interpreter/lexer/lexer.go
@@ -4,113 +4,144 @@ import (
 	"go-learning/interpreter/token"
 )
 
+type CharactersMap map[byte]func() token.Token
+
 type Lexer struct {
-	input        string
-	position     int  // current position in input (points to current currentChar)
-	readPosition int  // current reading position in input (after current currentChar)
-	currentChar  byte // current currentChar under examination
+	input       string
+	charPointer int  // points to the current char
+	currentChar byte // current char under examination
 }
 
 func New(input string) *Lexer {
 	lexer := &Lexer{input: input}
-	lexer.readCurrentChar()
+	lexer.readFirstChar()
 	return lexer
 }
 
-func (lexer *Lexer) readCurrentChar() {
-	lexer.currentChar = lexer.input[lexer.readPosition]
+func (lexer *Lexer) readFirstChar() {
+	lexer.currentChar = lexer.input[lexer.charPointer]
 }
 
 func (lexer *Lexer) readNextChar() {
-	lexer.readPosition += 1
-	if lexer.readPosition >= len(lexer.input) {
+	nexCharPointer := lexer.charPointer + 1
+	if nexCharPointer >= len(lexer.input) {
 		lexer.currentChar = 0
 	} else {
-		lexer.currentChar = lexer.input[lexer.readPosition]
+		lexer.currentChar = lexer.input[nexCharPointer]
 	}
-	lexer.position = lexer.readPosition
+	lexer.charPointer = nexCharPointer
 }
 
 func (lexer *Lexer) peekNextChar() byte {
-	if lexer.readPosition >= len(lexer.input) {
+	if lexer.charPointer >= len(lexer.input) {
 		return 0
 	}
-	return lexer.input[lexer.readPosition+1]
+	return lexer.input[lexer.charPointer+1]
 }
 
 func (lexer *Lexer) NextToken() token.Token {
 	var currentToken token.Token
 
+	var charactersMap CharactersMap
+	charactersMap = map[byte]func() token.Token{}
+
 	lexer.skipWhitespace()
 
-	switch lexer.currentChar {
-	case '=':
+	charactersMap['='] = func() token.Token {
 		if lexer.peekNextChar() == '=' {
 			lexer.readNextChar()
-			currentToken = token.Token{Type: token.EQUAL, Literal: token.EQUAL}
-		} else {
-			currentToken = createToken(token.ASSIGN, lexer.currentChar)
+			return token.Token{Type: token.EQUAL, Literal: token.EQUAL}
 		}
-	case '+':
-		currentToken = createToken(token.PLUS, lexer.currentChar)
-	case '-':
-		currentToken = createToken(token.MINUS, lexer.currentChar)
-	case '!':
+		return createToken(token.ASSIGN, lexer.currentChar)
+	}
+	charactersMap['+'] = func() token.Token {
+		return createToken(token.PLUS, lexer.currentChar)
+	}
+	charactersMap['-'] = func() token.Token {
+		return createToken(token.MINUS, lexer.currentChar)
+	}
+	charactersMap['!'] = func() token.Token {
 		if lexer.peekNextChar() == '=' {
 			currentChar := lexer.currentChar
 			lexer.readNextChar()
 			nextChar := lexer.currentChar
 			literal := string(currentChar) + string(nextChar)
-			currentToken = token.Token{Type: token.NOT_EQUAL, Literal: literal}
-		} else {
-			currentToken = createToken(token.BANG, lexer.currentChar)
+			return token.Token{Type: token.NOT_EQUAL, Literal: literal}
 		}
-	case '*':
-		currentToken = createToken(token.ASTERISK, lexer.currentChar)
-	case '/':
-		currentToken = createToken(token.SLASH, lexer.currentChar)
-	case '<':
-		currentToken = createToken(token.LT, lexer.currentChar)
-	case '>':
-		currentToken = createToken(token.GT, lexer.currentChar)
-	case ';':
-		currentToken = createToken(token.SEMICOLON, lexer.currentChar)
-	case ':':
-		currentToken = createToken(token.COLON, lexer.currentChar)
-	case '(':
-		currentToken = createToken(token.LPAREN, lexer.currentChar)
-	case ')':
-		currentToken = createToken(token.RPAREN, lexer.currentChar)
-	case ',':
-		currentToken = createToken(token.COMMA, lexer.currentChar)
-	case '{':
-		currentToken = createToken(token.LBRACE, lexer.currentChar)
-	case '}':
-		currentToken = createToken(token.RBRACE, lexer.currentChar)
-	case '[':
-		currentToken = createToken(token.LBRACKET, lexer.currentChar)
-	case ']':
-		currentToken = createToken(token.RBRACKET, lexer.currentChar)
-	case '"':
+		return createToken(token.BANG, lexer.currentChar)
+	}
+	charactersMap['*'] = func() token.Token {
+		return createToken(token.ASTERISK, lexer.currentChar)
+	}
+	charactersMap['/'] = func() token.Token {
+		return createToken(token.SLASH, lexer.currentChar)
+	}
+	charactersMap['<'] = func() token.Token {
+		return createToken(token.LT, lexer.currentChar)
+	}
+	charactersMap['>'] = func() token.Token {
+		return createToken(token.GT, lexer.currentChar)
+	}
+	charactersMap[';'] = func() token.Token {
+		return createToken(token.SEMICOLON, lexer.currentChar)
+	}
+	charactersMap[':'] = func() token.Token {
+		return createToken(token.COLON, lexer.currentChar)
+	}
+	charactersMap['('] = func() token.Token {
+		return createToken(token.LPAREN, lexer.currentChar)
+	}
+	charactersMap[')'] = func() token.Token {
+		return createToken(token.RPAREN, lexer.currentChar)
+	}
+	charactersMap[','] = func() token.Token {
+		return createToken(token.COMMA, lexer.currentChar)
+	}
+	charactersMap['{'] = func() token.Token {
+		return createToken(token.LBRACE, lexer.currentChar)
+	}
+	charactersMap['}'] = func() token.Token {
+		return createToken(token.RBRACE, lexer.currentChar)
+	}
+	charactersMap['['] = func() token.Token {
+		return createToken(token.LBRACKET, lexer.currentChar)
+	}
+	charactersMap[']'] = func() token.Token {
+		return createToken(token.RBRACKET, lexer.currentChar)
+	}
+	charactersMap['"'] = func() token.Token {
 		currentToken.Type = token.STRING
 		currentToken.Literal = lexer.readString()
-	case 0:
+		return currentToken
+	}
+	charactersMap[0] = func() token.Token {
 		currentToken.Literal = ""
 		currentToken.Type = token.EOF
-	default:
-		if isLetter(lexer.currentChar) {
-			currentToken.Literal = lexer.readIdentifier()
-			currentToken.Type = token.LookupIdent(currentToken.Literal)
-			return currentToken
-		} else if isDigit(lexer.currentChar) {
-			currentToken.Literal = lexer.readNumber()
-			currentToken.Type = token.INT
-			return currentToken
-		} else {
-			currentToken = createToken(token.ILLEGAL, lexer.currentChar)
-		}
+		return currentToken
 	}
+
+	if charHandler, ok := charactersMap[lexer.currentChar]; ok {
+		currentToken = charHandler()
+		lexer.readNextChar()
+		return currentToken
+	}
+
+	if isLetter(lexer.currentChar) {
+		currentToken.Literal = lexer.readIdentifier()
+		currentToken.Type = token.LookupIdent(currentToken.Literal)
+		return currentToken
+	}
+
+	if isDigit(lexer.currentChar) {
+		currentToken.Literal = lexer.readNumber()
+		currentToken.Type = token.INT
+		return currentToken
+	}
+
+	currentToken = createToken(token.ILLEGAL, lexer.currentChar)
+
 	lexer.readNextChar()
+
 	return currentToken
 }
 
@@ -121,41 +152,33 @@ func (lexer *Lexer) skipWhitespace() {
 }
 
 func (lexer *Lexer) readIdentifier() string {
-	identifierStartChar := lexer.position
+	identifierStartChar := lexer.charPointer
 	for isLetter(lexer.currentChar) {
 		lexer.readNextChar()
 	}
-	identifierEndChar := lexer.position
+	identifierEndChar := lexer.charPointer
 	return lexer.input[identifierStartChar:identifierEndChar]
 }
 
 func (lexer *Lexer) readNumber() string {
-	numberStartChar := lexer.position
+	numberStartChar := lexer.charPointer
 	for isDigit(lexer.currentChar) {
 		lexer.readNextChar()
 	}
-	numberEndChar := lexer.position
+	numberEndChar := lexer.charPointer
 	return lexer.input[numberStartChar:numberEndChar]
 }
 
 func (lexer *Lexer) readString() string {
-	stringStart := lexer.position + 1
+	stringStart := lexer.charPointer + 1
 	for {
 		lexer.readNextChar()
 		if lexer.currentChar == '"' || lexer.currentChar == 0 {
 			break
 		}
 	}
-	stringEnd := lexer.position
+	stringEnd := lexer.charPointer
 	return lexer.input[stringStart:stringEnd]
-}
-
-func isLetter(char byte) bool {
-	return 'a' <= char && char <= 'z' || 'A' <= char && char <= 'Z' || char == '_'
-}
-
-func isDigit(char byte) bool {
-	return '0' <= char && char <= '9'
 }
 
 func createToken(tokenType token.TokenType, currentChar byte) token.Token {
